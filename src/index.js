51919241b02537432a540e7693a8cf1106056d32
Ali Madooei <alimadooei@gmail.com>
2022-06-30 15:12:03 -0400

    Get rid of reportWebVital

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import 'assets/third-party/apex-chart.css';
 // project import
 import App from './App';
 import { store } from 'store';
-import reportWebVitals from './reportWebVitals';
 
 // ==============================|| MAIN - REACT DOM RENDER  ||============================== //
 
@@ -28,8 +27,3 @@ ReactDOM.render(
     </StrictMode>,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
