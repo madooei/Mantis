0d6e2c6c49bbdec4484a2bf100f91d979d19d774
Ali Madooei <alimadooei@gmail.com>
2022-06-30 12:14:08 -0400

    Remove Ant Icons component

--- src/menu-items/utilities.js
+++ src/menu-items/utilities.js
@@ -45,14 +45,6 @@ const utilities = {
             type: 'item',
             url: '/shadow',
             icon: icons.BarcodeOutlined
-        },
-        {
-            id: 'ant-icons',
-            title: 'Ant Icons',
-            type: 'item',
-            url: '/icons/ant',
-            icon: icons.AntDesignOutlined,
-            breadcrumbs: false
         }
     ]
 };
