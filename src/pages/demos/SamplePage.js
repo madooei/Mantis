0a8c021575728c9592bbb59eb74452c066e8e36b
Ali Madooei <alimadooei@gmail.com>
2022-06-30 22:31:34 -0400

    move the sample page

--- src/pages/demos/SamplePage.js
+++ src/pages/demos/SamplePage.js
@@ -1,10 +1,5 @@
-// material-ui
 import { Typography } from '@mui/material';
-
-// project import
 import MainCard from 'components/MainCard';
-
-// ==============================|| SAMPLE PAGE ||============================== //
 
 const SamplePage = () => (
     <MainCard title="Sample Card" codeHighlight>
